569368f12796b5c44c0c4fa9cd0c67b206559e1e
Bikram Thapa <bikramt755@gmail>
2025-01-28 08:23:42 +1100

    fetch the data

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,31 @@ import { useEffect, useState, useRef } from 'react'
 import * as d3 from 'd3'
 
 function App() {
- 
+ const [data, setData] = useState(null);
+ const svgRef = useRef();
+
+  useEffect(() => {
+    async function fetchData() {
+     try {
+      const response = await fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json');
+       if (response.ok) {
+        const data = await response.json();
+        setData(data);
+       }
+     } catch (error) {
+      console.error('Error fetching data: ', error);
+     }
+    }
+    fetchData();
+    console.log(data)
+  }, [])
+
+
+
+   
+
   return <>
-
-
+   <svg ref={svgRef}></svg>
     </>
 }
 
